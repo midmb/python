5615573d9db74654fddd337538583115753475ca
midmb <mibarbu@gmail.com>
2025-02-22 11:45:00 +0000

    Update and rename systemMonitor.py to SystemMonitor.py

---
+++ SystemMonitor.py
@@ -0,0 +1,31 @@
+
+This system monitor program provides:
+
+CPU monitoring:
+
+Usage per core
+Average CPU usage
+Current frequency
+Core count (physical and logical)
+
+
+Memory monitoring:
+
+Total memory
+Available memory
+Used memory
+Usage percentage
+
+
+Disk monitoring:
+
+All mounted partitions
+Total, used, and free space
+Usage percentage
+
+
+Network monitoring:
+
+Bytes sent/received
+Packets sent/received
+
